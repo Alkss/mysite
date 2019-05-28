da2f31987c6cc4312b7cf34367490d2f1f96418d
Alkss <alkss_fjv@yahoo.com.br>
2019-05-28 20:30:54 -0300

    education update

--- controler/js/main-page.js
+++ controler/js/main-page.js
@@ -1,30 +1,81 @@
 $(document).ready(function () {
     $("#workingXPBtn").click(function () {
-        $('html,body').animate({
-                scrollTop: $("#workingXP").offset().top-50
-            },
-            'slow');
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
+        workingBtn();
     });
 
     $("#skillBtn").click(function () {
-        $('html,body').animate({
-                scrollTop: $("#skills").offset().top-50
-            },
-            'slow');
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
+        skillBtn();
     });
 
     $("#languagesBtn").click(function () {
-        $('html,body').animate({
-                scrollTop: $("#languages").offset().top-50
-            },
-            'slow');
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
+        languagesBtn();
     });
 
+    $("#educationBtn").click(function () {
+        educationBtn();
+    });
+
+    $("#scrollTopBtn").click(function () {
+        scrollTopBtn();
+    });
+
+    $("#projectsBtn").click(function () {
+        //TODO: Implement modal
+    });
+
+    $("#contactBtn").click(function () {
+        //TODO: Implement modal
+    });
+
+    $("#aboutBtn").click(function () {
+        //TODO: Implement modal
+    });
 
 });
+
+
+function workingBtn() {
+    $('html,body').animate({
+            scrollTop: $("#workingXP").offset().top - 50
+        },
+        'slow');
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+}
+
+function scrollTopBtn() {
+    $('html,body').animate({
+            scrollTop: $("#perfil").offset().top - 50
+        },
+        'slow');
+}
+
+function skillBtn() {
+    $('html,body').animate({
+            scrollTop: $("#skills").offset().top - 50
+        },
+        'slow');
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+}
+
+function languagesBtn() {
+    $('html,body').animate({
+            scrollTop: $("#languages").offset().top - 50
+        },
+        'slow');
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+}
+
+function educationBtn() {
+    $('html,body').animate({
+            scrollTop: $("#education").offset().top - 50
+        },
+        'slow');
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+}
--- index.php
+++ index.php
@@ -2,3 +2,6 @@
 include 'view/header.html';
 include 'view/index.html';
 include 'view/footer.html';
+include 'view/about-modal.html';
+include 'view/contact-modal.html';
+include 'view/project-modal.html';
